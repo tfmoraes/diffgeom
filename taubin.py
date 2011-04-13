2154e2b7b6fdb81fe09e900771502ad0eac95e20
Thiago Franco de Moraes <totonixsame@gmail.com>
2011-04-13 00:04:55 -0300

    Alguns testes

--- taubin.py
+++ taubin.py
@@ -1,10 +1,9 @@
 import sys
 
-import numpy
+import numpy as np
 import vtk
 
 from vtk.util import numpy_support
-from scipy.ndimage import gaussian_filter1d
 
 def load_file(filename):
     if filename.endswith('.ply'):
@@ -16,42 +15,36 @@ def load_file(filename):
     return reader.GetOutput()
 
 def calculate_d(mesh, pid):
-    tx, ty, tz = 0, 0, 0
+    t = 0
     n = 0.0
     cell_ids = vtk.vtkIdList()
-    p = mesh.GetPoint(pid)
+    p0 = np.array(mesh.GetPoint(pid))
     mesh.GetPointCells(pid, cell_ids)
     for i in xrange(cell_ids.GetNumberOfIds()):
         point_ids = vtk.vtkIdList()
         mesh.GetCellPoints(cell_ids.GetId(i), point_ids)
         n += 1
         if point_ids.GetId(0) != pid:
-            np = mesh.GetPoint(point_ids.GetId(0))
+            p1 = np.array(mesh.GetPoint(point_ids.GetId(0)))
         else:
-            np = mesh.GetPoint(point_ids.GetId(1))
+            p1 = np.array(mesh.GetPoint(point_ids.GetId(1)))
 
-        tx = tx + (np[0] - p[0])
-        ty = ty + (np[1] - p[1])
-        tz = tz + (np[2] - p[2])
+        t = t + (p1 - p0)
 
-    tx, ty, tz =  tx / n, ty / n, tz / n
-    return tx, ty, tz
-
-        
+    return t / n
 
 def taubin_smooth(poly, l, m, steps):
-    trianglefilter = vtk.vtkTriangleFilter()
-    trianglefilter.SetInput(poly)
-    trianglefilter.Update()
-
     edgesfilter = vtk.vtkExtractEdges()
-    edgesfilter.SetInput(trianglefilter.GetOutput())
+    edgesfilter.SetInput(poly)
     edgesfilter.Update()
 
     edges = edgesfilter.GetOutput()
 
     new_poly = vtk.vtkPolyData()
-    new_poly.DeepCopy(trianglefilter.GetOutput())
+    new_poly.DeepCopy(poly)
+
+    print edges.GetNumberOfPoints()
+    print poly.GetNumberOfPoints()
 
     points = new_poly.GetPoints()
     for s in xrange(steps):
@@ -59,23 +52,31 @@ def taubin_smooth(poly, l, m, steps):
         for i in xrange(edges.GetNumberOfPoints()):
             D[i] = calculate_d(edges, i)
         for i in xrange(poly.GetNumberOfPoints()):
-            x, y, z = points.GetPoint(i)
-            x = x + l*D[i][0]
-            y = y + l*D[i][1]
-            z = z + l*D[i][2]
-            points.SetPoint(i, x, y, z)
+            p = np.array(points.GetPoint(i))
+            pl = p + l*D[i]
+            nx, ny, nz = pl
+            points.SetPoint(i, nx, ny, nz)
 
         D = {}
         for i in xrange(edges.GetNumberOfPoints()):
             D[i] = calculate_d(edges, i)
         for i in xrange(poly.GetNumberOfPoints()):
-            x, y, z = points.GetPoint(i)
-            x = x + m*D[i][0]
-            y = y + m*D[i][1]
-            z = z + m*D[i][2]
-            points.SetPoint(i, x, y, z)
+            p = np.array(points.GetPoint(i))
+            pl = p + m*D[i]
+            nx, ny, nz = pl
+            points.SetPoint(i, nx, ny, nz)
 
-    new_poly.SetPoints(points)
+        #D = {}
+        #for i in xrange(edges.GetNumberOfPoints()):
+            #D[i] = calculate_d(edges, i)
+        #for i in xrange(poly.GetNumberOfPoints()):
+            #x, y, z = points.GetPoint(i)
+            #nx = x + m*D[i][0]
+            #ny = y + m*D[i][1]
+            #nz = z + m*D[i][2]
+            #points.SetPoint(i, nx, ny, nz)
+
+    #new_poly.SetPoints(points)
     return new_poly
     
 
