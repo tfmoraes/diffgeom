7ffee7f6bed1228cf7a6052d75aa630c05e6961c
Thiago Franco de Moraes <totonixsame@gmail.com>
2011-05-29 21:54:46 -0300

    Caminhando ...

--- delaunay.py
+++ delaunay.py
@@ -1,6 +1,8 @@
 import numpy as np
 import math
 
+import winged_edge
+
 class Triangle(object):
     def __init__(self, p0, p1, p2):
         self.p0 = p0
@@ -40,13 +42,16 @@ class Tree(object):
 
 class DelaunayTriangulation(object):
     def __init__(self, points):
-        self.points = points
+        self.points = dict(zip(xrange(len(points)), points))
+        self.winged_edge = winged_edge.WingedEdge(self.points)
 
     def make_delaunay_triangulation(self):
-        self.points.sort(key=lambda x: [x[1], x[0]])
-        p0 = self.points[-1]
-        p_1 = self.points[0] - 1, 1000
-        p_2 = self.points[-1] + 1, -1000
+        points = self.points.values()
+        points.sort(key=lambda x: [x[1], x[0]])
+
+        p0 = points[-1]
+        p_1 = points[0] - 1, 1000
+        p_2 = points[-1] + 1, -1000
 
         t0 = Triangle(p0, p_1, p_2)
         self.tree = Tree(t0)
@@ -57,3 +62,12 @@ class DelaunayTriangulation(object):
         for r in vertices:
             self.tree.add(r)
 
+
+def main():
+    import random
+    points = [(random.random(), random.random()) for i in xrange(10)]
+
+    t = DelaunayTriangulation(points)
+
+if __name__ == '__main__':
+    main()
